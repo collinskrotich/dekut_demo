448d12d12a96a5356f6feb273583a562c915b770
Collins Rotich <14885955+collinskrotich@users.noreply.github.com>
2023-07-24 23:03:48 +0300

    pptx added

--- app/page.js
+++ app/page.js
@@ -5,18 +5,17 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center  p-24">
 DEKUT DEMO- When your fridge becomes your personal nutritionist by Collins Rotich
 <div className='pt-10'>
-        <ul>
-        <li>Demo slides</li>
-        
-        
+        <ol class="list-decimal list-inside space-y-5">
+        <li><a href="/dekut-saf-livedemo-fridge.pptx" download>Download PowerPoint</a></li>
+              
         <li><a href="https://docs.arduino.cc/tutorials/mkr-wifi-1010/securely-connecting-an-arduino-mkr-wifi-1010-to-aws-iot-core" target="_blank">Connecting Arduino to AWS IOT Core</a> </li>
 
         <li><a href="https://iot-temperature-tag.vercel.app/" target="_blank">Demo Vercel Link</a> </li>
 
-        <li><a href="https://github.com/collinskrotich/iot_temperature_tag" target="_blank">Github link</a> </li>
+        <li><a href="https://github.com/collinskrotich/iot_temperature_tag" target="_blank">Demo Github link</a> </li>
        
         <li> <a href = "https://www.linkedin.com/in/collins-rotich/" target='_blank'>Lets Connect on LinkedIn</a></li>
-      </ul>
+      </ol>
 </div>
     </main>
   )
